87701dbff7e909f74b03463a58d0d356d394a263
JustArchi <JustArchi@JustArchi.net>
2017-09-05 06:53:08 +0200

    Add encode/decode functions

--- Mods/Touhou Giveaways Event/Lua/Encounters/Cirno.lua
+++ Mods/Touhou Giveaways Event/Lua/Encounters/Cirno.lua
@@ -38,6 +38,54 @@ Counter = 0
 CustomAttack = 0
 SnowmanHP = 3
 
+FullBase64Characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/' -- This is temporary
+
+function GetEncodingCharacters(stringTable)
+	local b = ""
+	for i=#stringTable,1,-1 do
+		local str = stringTable[i]
+		for c in str:gmatch"." do
+			if (string.find(b, c) == nil) then
+				b = b .. c
+			end
+		end
+	end
+
+	return b
+end
+
+function Encode(data, stringTable)
+	local b = GetEncodingCharacters(stringTable)
+
+	return ((data:gsub('.', function(x) 
+		local r,b='',x:byte()
+		for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
+		return r;
+	end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
+		if (#x < 6) then return '' end
+		local c=0
+		for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
+		return b:sub(c+1,c+1)
+	end)..({ '', '==', '=' })[#data%3+1])
+end
+
+function Decode(data, stringTable)
+	local b = GetEncodingCharacters(stringTable)
+
+	data = string.gsub(data, '[^'..b..'=]', '')
+	return (data:gsub('.', function(x)
+		if (x == '=') then return '' end
+		local r,f='',(b:find(x)-1)
+		for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+		return r;
+	end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+		if (#x ~= 8) then return '' end
+		local c=0
+		for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+		return string.char(c)
+	end))
+end
+
 function EncounterStarting()
 	Player.name = "konrads6"
 	Player.lv = 3
